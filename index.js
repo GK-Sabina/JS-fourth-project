fe6974ad513d0a38e2994a46dbf97609f415bfb5
Sabina <justkrupasabina@gmail.com>
2023-07-25 23:03:59 +0300

    Add recursion, throw, try_catch_finally

--- index.js
+++ index.js
@@ -1,72 +1,99 @@
 'use strict';
 
-//expression, обычная функция
-const test2 = function(){
-    console.log(this);
-};
+//recursion
+// function recursion(number) {
+//     if(number===5){  //базовый случай
+//         return;
+//     }
+//     console.log(number)
+//     recursion(number+1);
+// }
 
-// //syntaxis arrow function, стрелочная функция
-const test3 = () => {
-    console.log(this);
-};
+// recursion(0); //0,1,2,3,4 когда доходим до 5 - выходим тк прописан базовый случай
 
-test2(1, 2, 3);
-test3(1, 2, 3);
+// function getPowerNumber (number, exp){
+// let result = 1;
+// for (let i = 0; i < exp; i++){
+//     result *= number;
+// }
+// return result;
+// }
+// getPowerNumber(2, 3); //8
 
-console.dir(test2);
-console.dir(test3);
+//Task рекурсия для нахождения степени числа
+// function getPowerNumberRecursion(number, exp){
+//     if(exp === 0){
+//         return 1;
+//     }
+//     if(exp === 1){
+//         return number;
+//     }
+//     return number * getPowerNumberRecursion(number, exp - 1); //exp - 1 это не модифицированное значение, а новое
+// }
+// console.log(getPowerNumberRecursion(2, 4)); 
 
-//bind
-function showHeader(header){
-    console.log(this); //undefined
-    console.log(header, this.title);
-}
+// //Task стрелочная функция
+// const getPowerNumberRecursion = (number, exp) => (exp === 0) ? 1: number * getPowerNumberRecursion(number, exp - 1);
 
-const site = {
-    title: 'Portfolio',
-    headers: ['Welcome', 'About me', 'Contacts'],
-    showHeader(){
-        this.headers.forEach(showHeader.bind(this)); //this === site, bind возвращает новую функцию в которой вместо this прописался site
-    }
-}
+// console.log(getPowerNumberRecursion(2, 3)); 
+
+//throw
+/**
+ * 
+//  * @param {number} number 
+//  * @param {number} exp 
+//  * @returns 
+//  */
+
+// function getPowerNumberRecursion(number, exp){
+//         if(typeof number !== 'number' || typeof exp !== 'number'){
+//             throw new TypeError('type incorrect'); //создаем ошибку с помощью throw
+//         }
+//         if(exp < 0 || exp >= 100){
+//             throw new RangeError('exp must be positive less 100');
+//         }
+//         if(exp === 0){
+//             return 1;
+//         }
+//         if(exp === 1){
+//             return number;
+//         }
+//         return number * getPowerNumberRecursion(number, exp - 1); //exp - 1 это не модифицированное значение, а новое
+//     }
+//     console.log(getPowerNumberRecursion(2, 4)); 
+    
+//     //как ловить ошибки и обрабатывать их, try catch finally
+//     try{
+//         console.log(getPowerNumberRecursion(2, 'qwe'));
+//     } catch (error) {
+//         console.log('error -', error)
+//         if(error instanceof RangeError){
+//             alert('check exp!')
+//         } else if(error instanceof TypeError){
+//             alert('check type!')
+//         } else{
+//             alert('error')
+//         }
+//     } finally {
+//         console.log('finally')
+//     }
+
+//     console.log('more code');
+
+//HOMEWORK Recursion_try_catch
+//Написати рекурсивну функцію, яка обчислює факторіал числа (number).
+//*Функція повинна приймати ціле число з типом данних number або bigint
+//Кидатися посилками і викликати функцію в конструкції try/catch/finally
+
+//если использовать цикл for вместо рекурсии
+// function getFactorialNumber(number){
+//    let sum = 1;
+
+//    for (let i = 2; i <= number; i++) {
+//     sum = sum * i;
+//    }
+
+//    return sum;
+// }
 
 
-//Task стрелочная функция 
-// const getSqure = (number=1) => number * number; //вычисляем площадь с помощью стрелочной функции
-
-// const logSqure = (number=1) => console.log(number * number);
-// const result = logSqure(5);
-// console.log(result);
-
-// const returnObject = (value) => ({prop: value})
-// const result2 = returnObjects(5);
-// console.log(result2);
-
-
-//...rest
-const test4 = (...args) => {
-    console.log(args); //помести все аргументы переданные в функцию в массив
-}
-
-//Tast ...rest написать функцию которая считает сумму всех переданных параметров
-const sum = (...args) => {
-    let summa = 0;
-    // for (let index = 0; index > args.length; index ++){
-    //     summa += args(index);
-    // } 
-    //или
-    args.forEach((arg) => {
-        summa += arg
-    });
-    return summa
-};
-
-const result = sum(1, 2, 3, 1); //7
-// c использованием reduce
-const sumWithReduce = (...args) => args.reduce((summa, arg)=>summa + arg, 0) //accumulitor - это аккумулирующий значение, которое возвращает функция callback после посещения очередного элемента используется только для reduce, summa === accumulitor, 0 === начальный элемент
-const result2 = sumWithReduce(1, 2, 3,1); //7
-const result3 = sumWithReduce(); //0
-
-//spread, посчитать сумму элементов массива
-const arr = [1,2,3,4,5];
-const result4 = sum(...arr); // === sum(1,2,3,4,5); //15
